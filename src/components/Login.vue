5a0440ad1969d16cbfef5a7e7e8130f62f0699c8
paypulse <sourcedev5@gmail.com>
2021-06-24 18:11:10 +0900

    2021_06_24_add cordova_화면 구성 및 로컬 앱 연동

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="page-container">
+    <div class="login-form-container shadow">
+      <div class="login-form-right-side">
+        <div class="top-logo-wrap">
+
+        </div>
+        <h1>Login Page</h1>
+        <p>구조를 잡기 위한 로그인 페이지 예시입니다.</p>
+      </div>
+      <div class="login-form-left-side">
+        <div class="login-top-wrap">
+          <span>Don't have an account?</span>
+          <button class="create-account-btn shadow-light">Create Profile</button>
+        </div>
+        <div class="login-input-container">
+          <div class="login-input-wrap input-id">
+            <i class="far fa-envelope"></i>
+            <input placeholder="Email" type="text">
+          </div>
+          <div class="login-input-wrap input-password">
+            <i class="fas fa-key"></i>
+            <input placeholder="Password"  type="password">
+          </div>
+        </div>
+        <div class="login-btn-wrap">
+          <button class="login-btn" v-on:click="login">Login</button>
+          <a href="#" >Forgot password?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+  },
+  methods: {
+    login: function(){
+        this.$router.push('/mainPage');
+    }
+  }
+
+}
+
+</script>
---
+++ src/components/mainPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page-container">
+    <div class="login-form-left-side">
+      <div class="login-btn-wrap">
+
+        <button class="login-btn" v-on:click="study">Study</button>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'mainPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+  },
+  methods:{
+    study: function(){
+
+      //기기 테스트
+      let userAgent = navigator.userAgent;
+      let anroidAppUrl = "myappandroi://main_web";
+
+
+      //browser 일 경우
+      let browser = userAgent.match(/Windows/);
+      if(browser){
+        this.$router.push('/studySample');
+      }
+
+      //ios 일 경우
+      let iosV = userAgent.match(/iphon|iPad|iPod/);
+      //android 일 경우
+      let androidV = userAgent.match(/Android/);
+
+      if(androidV){
+        //chrome browser에서 동작하는지
+
+        if(userAgent.match(/Chrome/)){
+          alert("clcik");
+          location.href = "intent://edulab#Intent;scheme=htmltest;package=com.edulab.htmltest;end";
+
+        }
+
+      }
+
+
+    }
+  }
+
+}
+
+</script>
